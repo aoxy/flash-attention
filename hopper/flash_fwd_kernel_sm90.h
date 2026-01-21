14d3d19ef18b9be15f640f2bf63c29bc81b76864
aoxy <aoxym@qq.com>
2026-01-21 20:13:54 +0800

    Remove log.

--- hopper/flash_fwd_kernel_sm90.h
+++ hopper/flash_fwd_kernel_sm90.h
@@ -415,7 +415,6 @@ public:
                             int num_splits_dynamic = reinterpret_cast<int&>(num_splits_dynamic_u);
                             if (num_splits_dynamic <= 1) {
                                 sink_val = params.mainloop.ptr_Sink[bidh];
-                                printf("Sink val: %f, Split: %d, Varlen:%d, num_splits_dynamic: %d\n", sink_val, int(Split), int(Varlen), num_splits_dynamic);
                             }
                         }
                     } else {
